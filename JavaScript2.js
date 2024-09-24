78aae6e7ef3b5c87b95508cd1c9891ee4078b2a1
Matvey Shekh <shekhmatvey@yandex.by>
2024-09-24 23:09:15 +0300

    .

---
+++ JavaScript2.js
@@ -0,0 +1,43 @@
+﻿function generateRandomArray(size) {
+    const array = [];
+    for (let i = 0; i < size; i++) {
+        array.push(Math.floor(Math.random() * 100) + 1);
+    }
+    return array;
+}
+
+function displayArray(array) {
+    const container = document.getElementById('array-container');
+    container.innerHTML = '';
+    array.forEach(value => {
+        const number = document.createElement('div');
+        number.style.margin = '0 5px';
+        number.textContent = value;
+        container.appendChild(number);
+    });
+}
+
+function bubbleSort(array) {
+    let i = 0;
+    let j = 0;
+    const interval = setInterval(() => {
+        if (i < array.length) {
+            if (j < array.length - i - 1) {
+                if (array[j] > array[j + 1]) {
+                    [array[j], array[j + 1]] = [array[j + 1], array[j]];
+                    displayArray(array);
+                }
+                j++;
+            } else {
+                j = 0;
+                i++;
+            }
+        } else {
+            clearInterval(interval);
+        }
+    }, 100);
+}
+
+const array = generateRandomArray(20);
+displayArray(array);
+bubbleSort(array);
